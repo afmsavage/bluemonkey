faf70fc81272785b1d6686ffc20464ae8aca8d4e
Antonio Savage <afmsavage@gmail.com>
2020-06-17 20:46:58 -0500

    wsl2 config chnges.

--- dynamodb.py
+++ dynamodb.py
@@ -4,15 +4,15 @@
 # Testing out some DynamoDB calls through boto3
 import boto3
 dynamodb = boto3.resource('dynamodb')
-  table = dynamodb.create_table(
-    TableName = 'labels',
-    KeySchema=[
-      {
-        'AttributeName': 'label',
-        'KeyType': 'S'
-      },
-      {
-        'AttributeName': ''
-      }
-    ]
-  )
+table = dynamodb.create_table(
+  TableName = 'labels',
+  KeySchema=[
+    {
+      'AttributeName': 'label',
+      'KeyType': 'S'
+    },
+    {
+      'AttributeName': ''
+    }
+  ]
+)
